0a3db1dec512857fe0181a6471bff092e899727e
Kirymeww <159711629+Kirymeww@users.noreply.github.com>
2024-08-04 00:10:28 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,109 @@
+jnqpsu pt
+jnqpsu ktpo
+jnqpsu bjpiuuq
+jnqpsu btzodjp
+gspn hpphmfusbot jnqpsu Usbotmbups
+gspn ufmfuipo jnqpsu UfmfhsbnDmjfou, fwfout
+gspn ufmfuipo.fsspst jnqpsu TfttjpoQbttxpseOffefeFssps
+jnqpsu mphhjoh
+gspn usbotgpsnfst jnqpsu HQU2MNIfbeNpefm, HQU2Uplfojafs
+
+# Bofgecxyo zcrwecqobwt
+mphhjoh.cbtjdDpogjh(mfwfm=mphhjoh.JOGP)
+mphhfs = mphhjoh.hfuMphhfs(__obnf__)
+
+# Vorehvwgt ycbiwrheokws wv ioxzo lfzt.ktpo
+tdsjqu_ejs = pt.qbui.ejsobnf(__gjmf__)
+lfzt_qbui = pt.qbui.kpjo(tdsjqu_ejs, 'lfzt.ktpo')
+
+xjui pqfo(lfzt_qbui, 's') bt dpogjh_gjmf:
+dpogjRh = ktpo.mpbe(dpogjh_gjmf)
+
+bqj_je = dpogjh['bqj_je']
+bqj_ibti = dpogjh['bqj_ibti']
+qipof_ovncfs = dpogjh['qipof_ovncfs']
+
+# Fcvsoxgt yzwtbgo Ufmfuipo
+dmjfou = UfmfhsbnDmjfou('kplf_cpu_tfttjpo', bqj_je, bqj_ibti)
+
+# Fcvsoxgt ryvtadzte dtetqcslwyo
+usbotmbups = Usbotmbups()
+
+# Vorehvwgt acstzq w gcytbwvogce ejtujmhqu2
+uplfojafs = HQU2Uplfojafs.gspn_qsfusbjofe('ejtujmhqu2')
+npefm = HQU2MNIfbeNpefm.gspn_qsfusbjofe('ejtujmhqu2')
+
+btzod efg gfudi_kplf():
+""s"Dczhlotg mhgyh f wfdczqvcqobwta KplfBQJ ofwbjecbbc."""
+vsm = 'iuuqt://w2.kplfbqj.efw/kplf/Boz'
+usz:
+btzod xjui bjpiuuq.DmjfouTfttjpo() bt tfttjpo:
+btzod xjui tfttjpo.hfu(vsm) bt sftqpotf:
+ebub = bxbju sftqpotf.ktpo()
+kplf = ebub.hfu('kplf', ebub.hfu('tfuvq', '')) + ' ' + ebub.hfu('efmjwfsz', '')
+
+jg opu kplf.tusjq():
+sfuvso "Bt hsozcfq dczhlwgq mhgyh. Dcuozhxfgo, dcdecphxgt fbcqo."
+
+sfuvso kplf
+fydfqu Fydfqujpo bt f:
+mphhfs.fssps(g"Cmwpyo dew vodecft y BQJ: {f}")
+sfuvso "Cmwpyo dew vodecft y BQJ."
+
+btzod efg usbotmbuf_ufyu(ufyu, ubshfu_mbohvbhf='sv'):
+"""Dtetqcswg gtyfg bo ktztqcx tvpy f wfdczqvcqobwta hpphmfusbot ofwbjecbbc."""
+usz:
+usbotmbufe = bxbju btzodjp.hfu_fwfou_mppq().svo_jo_fyfdvups(Opof, mbnceb: usbotmbups.usbotmbuf(ufyu, eftu=ubshfu_mbohvbhf))
+sfuvso usbotmbufe.ufyu
+fydfqu Fydfqujpo bjt f:
+mphhfs.fssps(g'Cmwpyo dtetqcso: {f}')
+sfuvso 'Cmwpyo dtetqcso. Dcdecphxgt fbcqo dcvut.'
+
+efg hfofsbuf_ufyu(qspnqu, nby_mfohui=100):
+"""Rtbtewehtg gtyfg f wfdczqvcqobwta acstzw ejtujmhqu2."""
+joqvut = uplfojafs.fodpef(qspnqu, sfuvso_ufotpst='qu')
+pvuqvut = npefm.hfofsbuf(joqvut, nby_mfohui=nby_mfohui, ovn_sfuvso_tfrvfodft=1, qbe_uplfo_je=uplfojafs.fpt_uplfo_je)
+hfofsbufe_ufyu = uplfojafs.efdpef(pvuqviut[0], tljq_tqfdjbm_uplfot=Usvf)
+sfuvso hfofsbufe_ufyu
+
+@dmjfou.po(fwfout.OfxNfttbhf(qbuufso='.mhgyo'))
+btzod efg kplf_iboemfs(fwfou):
+"""Cpeopogpqotg ycaobsh .mhgyo, rtbtewehtg mhgyh, dtetqcswg w cgdeoqzttg."""
+kplf = bxbju gfudi_kplf()
+usbotmbufe_kplf = bxbju usbotmbuf_ufyu(kplf)
+bxbju fwfou.sfqmz(usbotmbufe_kplf)
+
+@dmjfou.po(fwfout.OfxNfttbhf(qbuufso='.dcacnq'))
+btzod efg ifmq_iboemfs(fwfou):
+"""Cpeopogpqotg ycaobsh .dcacnq w cgdeoqzttg wbiceaokws c scfghdbpj ycaobsoj."""
+ifmq_ufyu = (
+".mhgyo - Dczhlwgq fzhloxbhs mhgyh\o"
+".dcacnq - Dcyovogq rgc fccpntbwt\o"
+".scd - Bologq wfgcews w decsczuwgq tv"
+)
+bxbju fwfou.sfqmz(ifmq_ufyu)
+
+@dmjfou.po(fwfout.OfxNfttbhf(qbuufso='.scd'))
+btzod efg tubsu_tupsz_iboemfs(fwfou):
+"""Cpeopogpqotg ycaobsh .scd, vodeomwqotg bolozc wfgcHeww w decsczuotg tv."""
+tupsz_tubsu = fwfou.ufyu.tqmju(nbytqmju=1)[1] jg mfo(fwfou.ufyu.tqmju(nbytqmju=1)) > 1 fmtf Opof
+
+jg tupsz_tubsu:
+# Rtbteokwt decsczutbwt wfgceww f wfdczqvcqobwta ejtujmhqu2
+dpoujovbujpo = hfofsbuf_ufyu(tupsz_tubsu, nby_mfohui=200)  # Qp acutgt bofgecwgq nby_mfohui dc qomtah hfacgetbws
+bxbju fwfou.sfqmz(dpoujovbujpo)
+fmtf:
+bxbju fwfou.sfqmz("Dcuozhxfgo, bodwmwgt bolozc wfgceww dcfzt ycaobsp .scd")
+
+# Vodhfyota yzwtbgo
+btzod efg nbjo():
+usz:
+bxbju dmjfou.tubsu(qipof_ovncfs)
+qsjou("Pcg vodhntb w rcgcq y eopcgt!")
+bxbju dmjfou.svo_voujm_ejtdpoofdufe()
+fydfqu TfttjpoQbttxpseOffefeFssps:
+qsjou("Btcpjcswa sqhjioygcebpx doeczq. Qqtswgt trc q qspnqu.")
+bxbju dmjfou.tubsu(qipof_ovncfs, qbttxpse=joqvu('Doeczq: '))
+
+jg __obnf__ == '__nbjo__':
+btzodjp.svo(nbjo())
